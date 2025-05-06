8a87042639b2cab0973f2050e07e0c8f9591dbc4
Nino Rottier <73556479+steno3@users.noreply.github.com>
2025-05-06 08:29:12 +0200

    feat: fps counter

--- scripts/using_yolo.py
+++ scripts/using_yolo.py
@@ -22,11 +22,16 @@ def screen_capture():
 # Process the screen frames in a loop
 cv2.namedWindow("YOLO Detection", cv2.WINDOW_NORMAL)  # Create a window to display results
 cv2.resizeWindow("YOLO Detection", 800, 600)  # Resize the window
+time = cv2.getTickCount()  # Get the current time in ticks  # Initialize time variable for FPS calculation
 while True:
     frame = screen_capture()  # Capture a frame from the screen
     results = model(frame)  # Run the model on the captured frame
     annotated_frame = results[0].plot()  # Annotate the frame with detection results
+    # Annote frame with fps counter
+    fps = int(cv2.getTickFrequency() / (cv2.getTickCount() - time))  # Calculate FPS
+    cv2.putText(annotated_frame, f"FPS: {fps}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4)  # Add FPS text to the frame
     cv2.imshow("YOLO Detection", annotated_frame)  # Show the detection results in the single window
+    time = cv2.getTickCount()  # Get the current time in ticks
     if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty("YOLO Detection", cv2.WND_PROP_VISIBLE) < 1:  # Exit on 'q' key press or window close
         break
 
